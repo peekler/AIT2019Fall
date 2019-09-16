feea10e5b67111cbc03d7a103312d4c95e928b56
Peter Ekler <peter.ekler@gmail.com>
2019-09-16 20:33:28 +0200

    AITHello with Snackbar and Kotlin class

---
+++ AITHello/app/src/main/java/hu/ait/aithello/Car.kt
@@ -0,0 +1,12 @@
+package hu.ait.aithello
+
+class Car(val type: String){
+
+   init {
+
+   }
+
+    fun showType() : String {
+        return type
+    }
+}
--- AITHello/app/src/main/java/hu/ait/aithello/MainActivity.kt
+++ AITHello/app/src/main/java/hu/ait/aithello/MainActivity.kt
@@ -2,7 +2,9 @@ package hu.ait.aithello
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
@@ -12,14 +14,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Log.d("MainActivity", "MainActivity has started")
+
+        var myCar = Car("Tesla Model X")
+
         btnShow.setOnClickListener {
-            var text = etName.text.toString() + " " + Date(System.currentTimeMillis()).toString()
+            var a = 5/1
+
+            var welcome = getString(R.string.text_welcome) + "value $a"
+
+            var text = welcome + " " + etName.text.toString() + " " + Date(System.currentTimeMillis()).toString()
 
             tvData.text = text
 
-            Toast.makeText(this@MainActivity,
-                text,
-                Toast.LENGTH_LONG).show()
+            Log.d("MainActivity", "text is: $text")
+
+            //Toast.makeText(this@MainActivity,
+            //    text,
+            //    Toast.LENGTH_LONG).show()
+
+
+            Snackbar.make(layoutMain,
+                text, Snackbar.LENGTH_LONG).show()
         }
     }
 }
