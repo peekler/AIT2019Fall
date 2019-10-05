c0e79e1086fa5449e8aad0363b20164d5cdc0b05
Peter Ekler <peter.ekler@gmail.com>
2019-10-05 21:33:58 +0200

    Menu demo refactors

---
+++ BottomNavMenuDemo/app/src/main/java/hu/bme/aut/bottomnavfragmentsdemo/fragments/FragmentOne.kt
@@ -0,0 +1,27 @@
+package hu.bme.aut.bottomnavfragmentsdemo.fragments
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import hu.bme.aut.bottomnavfragmentsdemo.R
+import kotlinx.android.synthetic.main.fragment_one.view.*
+
+class FragmentOne: Fragment() {
+
+    companion object {
+        const val TAG="FragmentOne"
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val rootView=inflater.inflate(R.layout.fragment_one,container,false)
+
+        rootView.button.setOnClickListener {
+            Toast.makeText(activity, "DEMO", Toast.LENGTH_LONG).show()
+        }
+
+        return rootView
+    }
+}
